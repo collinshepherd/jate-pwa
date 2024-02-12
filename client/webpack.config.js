9f2c4d152fe5c58ef54108f82f583f18a74b7893
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2024-02-12 10:52:36 -0600

    Adding all config to webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,11 +2,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-const { webpack } = require("webpack");
-
-//  : Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -31,6 +28,19 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "Notetaking app with JavaScript syntax highlighting",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        public_path: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
       }),
     ],
 
@@ -39,6 +49,20 @@ module.exports = () => {
         {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
         },
       ],
     },
